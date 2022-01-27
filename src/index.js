b769ae887375caa5d13df069891f318274f392e4
Parish Neighbors <pn8bors@gmail.com>
2022-01-27 11:32:55 -0600

    First commit

--- src/index.js
+++ src/index.js
@@ -7,5 +7,21 @@ function changeColor(button) {
     } else if (current == 'orange') {
         button.style.setProperty('background-color', 'yellow');
         current = 'yellow';
+    } else if (current == 'yellow') {
+        button.style.setProperty('background-color', 'green');
+        current = 'green';
+    } else if (current == 'green') {
+        button.style.setProperty('background-color', 'blue');
+        current = 'blue';
+    } else if (current == 'blue') {
+        button.style.setProperty('background-color', 'purple');
+        current = 'purple';
+    } else if (current == 'purple') {
+        button.style.setProperty('background-color', 'red');
+        current = 'red';
+    } else {
+        button.style.setProperty('background-lolor', 'red');
+        current = 'red';
     }
+
 }
